d06bb1aca1726a1b362750778af9b76aefb6a981
Hightail <henslock123@gmail.com>
2023-01-26 16:03:51 -0500

    Update KillerAnnie.lua

--- KillerAIO/Champions/KillerAnnie.lua
+++ KillerAIO/Champions/KillerAnnie.lua
@@ -4,6 +4,7 @@ require "2DGeometry"
 require "GGPrediction"
 require "PremiumPrediction"
 require "KillerAIO\\KillerLib"
+require "KillerAIO\\KillerChampUpdater"
 
 scriptVersion = 1.25
 
@@ -14,51 +15,7 @@ end
 
 -- [ AutoUpdate ]
 
-do
-
-	local KILLER_PATH = COMMON_PATH.."KillerAIO/"
-	local KILLER_CHAMPS = KILLER_PATH.."Champions/"
-	local champName = myHero.charName
-	local champFile = "Killer"..champName..".lua"
-	local champVersion = "Killer"..champName..".version"
-	local gitHub = "https://raw.githubusercontent.com/Henslock/GoS-EXT/main/KillerAIO/Champions/"
-	
-    local function AutoUpdate()
-		local function FileExists(path)
-			local file = io.open(path, "r")
-			if file ~= nil then 
-				io.close(file) 
-				return true 
-			else 
-				return false 
-			end
-		end
-		
-		local function DownloadFile(path, fileName)
-			local startTime = os.clock()
-			DownloadFileAsync(gitHub .. fileName, path .. fileName, function() end)
-			repeat until os.clock() - startTime > 3 or FileExists(path .. fileName)
-		end
-        
-        local function ReadFile(path, fileName)
-            local file = io.open(path .. fileName, "r")
-            local result = file:read()
-            file:close()
-            return result
-        end
-        
-        DownloadFile(KILLER_CHAMPS, champVersion)
-        local NewVersion = tonumber(ReadFile(KILLER_CHAMPS, champVersion))
-        if NewVersion > scriptVersion then
-            DownloadFile(KILLER_CHAMPS, champFile)
-            print("New Killer Annie Version - Please reload with F6")
-		else
-			print("| KILLER | Annie Loaded! Enjoy :)")
-        end
-    end
-	
-   AutoUpdate()
-end
+UpdateMyHeroScript()
 
 ----------------------------------------------------
 --|                Champion               		|--
